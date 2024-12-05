40367b4fdfa11f75e86ff633adb0dc35b43787fd
Harm Manders <harmmanders@gmail.com>
2024-12-05 17:52:21 +0100

    update template

--- days/TEMPLATE/test.py
+++ days/TEMPLATE/test.py
@@ -1,24 +1,34 @@
 from utils.helpers import read_input
 from .solve import puzzleA, puzzleB
 
-def test(file, solution):
-    puzzle = file.split('/')[-1].split('.')[0]
+def test(puzzle, file, solution = None):
+    test_name = file.split('/')[-1].split('.')[0]
     test_data = read_input(file)
-    test_result = puzzleA(test_data)
+    test_result = puzzle(test_data)
 
     print("================================")
-    print(puzzle, end="\n\n")
-    print("input:")
-    print(test_data, end="\n\n")
-
-    print("assert:")
-    print(f"{solution} == {test_result}")
-    print("================================")
+    print(test_name, end="\n\n")
+    if (solution):
+        print("input:")
+        print(test_data, end="\n\n")
+        
+        print("assert:")
+        print(f"{solution} == {test_result}", end="\n\n")
+        assert test_result == solution, f"{test_name}: {test_result} != {solution}"
     
-    assert test_result == solution, f"{puzzle}: {test_result} != {solution}"
+        print(f"{test_name}: OK!")
+    else:
+        print(f"{test_name}: {test_result}")
+    print("================================", end="\n\n")
 
 def test_puzzleA(solution):
-    test('inputs/testA.txt', solution)
+    test(puzzleA, 'inputs/testA.txt', solution)
 
-def test_puzzleB():
-    test('inputs/testB.txt')
+def test_puzzleB(solution):
+    test(puzzleB, 'inputs/testB.txt', solution)
+
+def solve_puzzleA():
+    test(puzzleA, 'inputs/puzzleA.txt')
+
+def solve_puzzleB():
+    test(puzzleB, 'inputs/puzzleB.txt')
