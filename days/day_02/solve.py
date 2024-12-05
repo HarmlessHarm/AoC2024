b74cfdac55945a5be84dcac167ade2bfe6930160
Harm Manders <harmmanders@gmail.com>
2024-12-05 18:02:16 +0100

    start day 2

---
+++ days/day_02/solve.py
@@ -0,0 +1,11 @@
+from utils.helpers import read_input
+
+def main():
+    print(puzzleA(read_input('inputs/puzzleA.txt')))
+    print(puzzleB(read_input('inputs/puzzleB.txt')))
+
+def puzzleA(data):
+    return Exception("Not implemented")
+
+def puzzleB(data):
+    return Exception("Not implemented")
